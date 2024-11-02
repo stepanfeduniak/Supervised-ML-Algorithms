72358eda526790307c0bef941864eb26b2c19a47
Stepan Feduniak <stepanfedunyak@gmail.com>
2024-11-02 14:04:03 +0100

    Implement polynomial regression with training and testing - Added `train_model` function to create the design matrix and calculate regression coefficients - Conducted initial testing to validate predictions for polynomial regression

--- polynomial-regression/src/polynomial_regressor.py
+++ polynomial-regression/src/polynomial_regressor.py
@@ -1,10 +1,25 @@
-class LinearRegressor:
-    def __init__(self, analysed_data, predicted_data):
-        self.analysed_data=analysed_data
-        self.predicted_data=predicted_data
+import numpy as np
+import pandas as pd
 class PolynomialRegressor:
     polynomial_degree=1
-    def __init__(self,polynomial_degree, analysed_data, predicted_data):
-        self.analysed_data=analysed_data
-        self.predicted_data=predicted_data
+    def __init__(self,polynomial_degree):
         self.polynomial_degree=polynomial_degree
+    
+    def train_model(self, data, prediction):
+        data_array = data
+        prediction_data_array = prediction
+        #Creating an Vandermonde matrix
+        design_matrix_X=np.zeros((len(data), self.polynomial_degree + 1))
+        for i in range(self.polynomial_degree + 1):
+            design_matrix_X[:, i] = data_array ** i
+        print(design_matrix_X)
+        m1=design_matrix_X.T@design_matrix_X
+        b1=design_matrix_X.T@prediction
+        coefficient_vector= np.linalg.solve(m1,b1)
+        error_matrix= design_matrix_X@coefficient_vector -prediction
+        print(coefficient_vector)
+        print(error_matrix)
+
+    
+        
+        
--- polynomial-regression/src/prediction.py
+++ polynomial-regression/src/prediction.py
@@ -1,2 +1,8 @@
 from polynomial_regressor import PolynomialRegressor
-from polynomial_regressor import LinearRegressor
+import numpy as np
+np.set_printoptions(suppress=True, precision=4) 
+
+x = np.array([1, 33, 3, 4])
+y = np.array([1, 12, 3, 4])
+model1 = PolynomialRegressor(1)
+model1.train_model(x, y)
