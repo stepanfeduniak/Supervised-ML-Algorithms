5c2cb76405d6e45322e4dea6fd2fb92a28851047
Stepan Feduniak <stepanfedunyak@gmail.com>
2024-11-22 19:54:12 +0100

    Restructured the algorithm, joined two databases for more data

--- naive_bayes/src/naive_bayes.py
+++ naive_bayes/src/naive_bayes.py
@@ -1,33 +1,52 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import mean_absolute_error
-
+from collections import defaultdict
+from math import log
 class naive_bayes_algorithm:
     pass
 
 class spam_detector:
-    def __init__(self,polynomial_degree: int=1):
-        self.polynomial_degree = polynomial_degree
-        self.coefficient_vector = None
-        self.error_train = None
-    
-    def vandermonde(self, x: np.ndarray):
-        vandermonde_matrix=np.zeros((len(x), self.polynomial_degree + 1))
-        for i in range(self.polynomial_degree + 1):
-            vandermonde_matrix[:, i] = x ** i
-        return vandermonde_matrix
-    
-    def train_model(self, data, prediction):
-        design_matrix_X=self.vandermonde(data)
-        m1=design_matrix_X.T@design_matrix_X
-        b1=design_matrix_X.T@prediction
-        self.coefficient_vector= np.linalg.solve(m1,b1)
-        self.error_train= design_matrix_X@self.coefficient_vector -prediction
-        self.mean_error_train= mean_absolute_error(prediction,design_matrix_X@self.coefficient_vector)
+    def __init__(self,df):
+        self.df = df
         
+    def fit(self,type_1,type_2,classyfying_column,data_column):
+        self.spam_messages = self.df[self.df[classyfying_column] == type_2][data_column]
+        self.ham_messages = self.df[self.df[classyfying_column] == type_1][data_column]
+        spam_words=self.tokenise(self.spam_messages)
+        ham_words=self.tokenise(self.ham_messages)
+        self.value_list=self.word_counter(spam_words,ham_words)
+        print("Model is fit")
     
-    def predict(self, X):
-        prediction=np.zeros(len(X))
-        for i in range(self.polynomial_degree+1):
-            prediction=prediction+self.coefficient_vector[i]*(X**i)
-        return prediction
+    def word_counter(self, a,b):
+        word_counts = defaultdict(lambda: [0,0])
+        for word in a:
+            word_counts[word][0] += 1
+        for word in b:
+            word_counts[word][1] += 1
+        return word_counts
+    
+    def predict(self,phrase):
+        coef_bayes_spam = (len(self.spam_messages) / (len(self.spam_messages) + len(self.ham_messages)))
+        coef_bayes_ham = (len(self.ham_messages) / (len(self.spam_messages) + len(self.ham_messages)))
+        check_message = phrase.split()
+        log_prob_spam = log(coef_bayes_spam) 
+        log_prob_ham = log(coef_bayes_ham)   
+        for word in check_message:
+            in_spam = self.value_list.get(word, [0, 0])[0]
+            not_in_spam = self.value_list.get(word, [0, 0])[1]
+            prob_word_given_spam = (in_spam+1 ) / (len(self.spam_messages) + len(self.value_list)) 
+            prob_word_given_ham = (not_in_spam+1) / (len(self.ham_messages) + len(self.value_list)) 
+            log_prob_spam += log(prob_word_given_spam)
+            log_prob_ham += log(prob_word_given_ham)
+        if log_prob_spam > log_prob_ham:
+            return 'Spam'
+        else:
+            return 'Ham'
+
+
+
+
+    def tokenise(self,X):
+        a= X.apply(lambda x: x.split()).explode()
+        return a
+
--- naive_bayes/src/prediction.py
+++ naive_bayes/src/prediction.py
@@ -16,63 +16,16 @@ from math import log
 np.set_printoptions(suppress=True, precision=4)
 
 # Load data
-spam_df = pd.read_csv("./naive_bayes/data/spam.csv",encoding='ISO-8859-1')
+df1 = pd.read_csv("./naive_bayes/data/spam.csv",encoding='ISO-8859-1')
+df2 = pd.read_csv("./naive_bayes/data/spam_ham_dataset.csv",encoding='ISO-8859-1')
 
-# Define the pipeline for the Message column
-spam_messages = spam_df[spam_df['v1'] == 'spam']['v2']
-ham_messages = spam_df[spam_df['v1'] == 'ham']['v2']
+df1_cleaned = df1[['v1', 'v2']].rename(columns={'v1': 'Label', 'v2': 'Message'})
+df2_cleaned = df2.rename(columns={'Category': 'Label', 'Message': 'Message'})
 
-# Tokenize the messages (using simple split for now)
-spam_words = spam_messages.apply(lambda x: x.split()).explode()
-ham_words = ham_messages.apply(lambda x: x.split()).explode()
+merged_df = pd.concat([df1_cleaned, df2_cleaned], ignore_index=True)
 
-# Step 2: Create a dictionary to hold the word counts
-word_counts = defaultdict(lambda: [0,0])  # Default dictionary where [in_spam, not_in_spam]
-
-# Step 3: Count the occurrences of each word in spam and ham messages
-for word in spam_words:
-    word_counts[word][0] += 1  # Increment the 'in_spam' count
-
-for word in ham_words:
-    word_counts[word][1] += 1  # Increment the 'not_in_spam' count
-#for word, counts in word_counts.items():
- #   if counts[0]>counts[1]:
-  #      print(f"{word}: {counts[0]}, {counts[1]}")
-
-
-
-# Test message and prior probabilities (already given)
-test_message = "Wassup bro"
-coef_bayes_spam = (len(spam_messages) / (len(spam_messages) + len(ham_messages)))
-coef_bayes_ham = (len(ham_messages) / (len(spam_messages) + len(ham_messages)))
-check_message = test_message.split()
-
-# Initialize the log probabilities
-log_prob_spam = log(coef_bayes_spam)  # Start with the log of prior probability of spam
-log_prob_ham = log(coef_bayes_ham)    # Start with the log of prior probability of ham
-
-# Step 1: For each word in the message, calculate the likelihood of that word in spam and ham
-for word in check_message:
-    # Get the spam and ham counts for the word (defaulting to 0 if the word is not found)
-    in_spam = word_counts.get(word, [0, 0])[0]
-    not_in_spam = word_counts.get(word, [0, 0])[1]
-    
-    # Avoid division by zero by adding 1 (Laplace smoothing)
-    prob_word_given_spam = (in_spam+1 ) / (len(spam_words) + len(word_counts))  # Laplace smoothing
-    prob_word_given_ham = (not_in_spam+1) / (len(ham_words) + len(word_counts))  # Laplace smoothing
-    print((log(prob_word_given_ham),log(prob_word_given_spam)))
-    # Step 2: Update the log probabilities for spam and ham
-    log_prob_spam += log(prob_word_given_spam)
-    log_prob_ham += log(prob_word_given_ham)
-
-# Step 3: Compare the log probabilities and classify the message
-print(len(word_counts))
-print(log_prob_ham)
-print(log_prob_spam)
-
-if log_prob_spam > log_prob_ham:
-    print("The message is classified as Spam")
-else:
-    print("The message is classified as Ham")
-
-
+spam_detector_machine=spam_detector(merged_df)
+spam_detector_machine.fit("ham","spam","Label","Message")
+text_to_check="Wassup bro, I just won 1000 dollars by betting on football"
+result=spam_detector_machine.predict(text_to_check)
+print(result)
